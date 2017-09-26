c6013992c6ccd293be5615639d00149c06917093
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2017-09-26 15:44:39 -0700

    commenting

--- App.js
+++ App.js
@@ -21,8 +21,10 @@ export default class App extends React.Component {
     const width = gl.drawingBufferWidth;
     const height = gl.drawingBufferHeight;
 
+    // ar init
     // const arSession = await this._glView.startARSessionAsync();
 
+    // three.js init
     const scene = new THREE.Scene();
     // scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
     // const camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
@@ -30,22 +32,27 @@ export default class App extends React.Component {
     const renderer = ExpoTHREE.createRenderer({ gl });
     renderer.setSize(width, height);
 
+    // lights
     const dirLight = new THREE.DirectionalLight(0xffffff);
     dirLight.position.set(1, 1, 1);
     scene.add(dirLight);
     const ambLight = new THREE.AmbientLight(0x404040);
     scene.add(ambLight);
 
+    // cube
     const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
     const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     cube.position.z = -0.4;
     scene.add(cube);
 
+    // main loop
     const animate = () => {
+      // rotate cube
       cube.rotation.x += 0.07;
       cube.rotation.y += 0.04;
 
+      // end frame and schedule new one!
       renderer.render(scene, camera);
       gl.endFrameEXP();
       requestAnimationFrame(animate);
